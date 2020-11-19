65822a10ad6ccef9c0430bff9082d38c21709c5f
gokysn <gokysn98@gmail.com>
2020-11-20 02:01:50 +0300

    added drop columns

--- src/main/scala/org/ytumatmuhproject/DataCleaning.scala
+++ src/main/scala/org/ytumatmuhproject/DataCleaning.scala
@@ -1,10 +1,12 @@
 package org.ytumatmuhproject
 
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SparkSession}
 import org.apache.spark.sql.functions.{col}
 
 object DataCleaning {
+
+
   def main(args: Array[String]): Unit = {
     Logger.getLogger("org").setLevel(Level.ERROR)
 
@@ -24,14 +26,28 @@ object DataCleaning {
       .option("sep",",")
       .option("header","true")
       .option("inferSchema","true")
+      .option("mode","DROPMALFORMED")
       .load("C:/Users/gokys/Desktop/airbnb/total_data.csv")
 
-   val df2 =  dfFromFile
-      .drop("_c0","scrape_id","last_scraped","thumbnail_url","medium_url","xl_picture_url","neighbourhood_group_cleansed","license","jurisdiction-names")
-      .filter(col("experiences_offered") === "none")
 
-    df2.na.drop()
-       .show()
+    // okunan dataframe'e ilk bakış
+    println("\n Orijinal DF")
+    dfFromFile.show(20)
+
+
+   val df2 = dfFromFile
+     .drop("_c0","description","minimum_maximum_nights","maximum_minimum_nights","minimum_minimum_nights","maximum_maximum_nights","minimum_nights_avg_ntm","maximum_nights_avg_ntm","number_of_reviews_ltm","calculated_host_listings_count_entire_homes","calculated_host_listings_count_private_rooms","calculated_host_listings_count_shared_rooms","space","summary","name","scrape_id","last_scraped","thumbnail_url","medium_url","xl_picture_url","neighbourhood_group_cleansed","license","jurisdiction-names")
+     .filter(col("experiences_offered") === "none")
+     .orderBy("id")
+
+    df2
+      .select("id", "amenities")
+      .filter(col("experiences_offered") === "none")
+      .groupBy("id","amenities")
+      .count()
+      .orderBy("id")
+      .show(false)
+
 
   }
 }
